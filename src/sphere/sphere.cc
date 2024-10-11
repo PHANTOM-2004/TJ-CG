55b54309ec6a5dcad0a28900287c1274a875e7ce
scarlet-arch <phantom190514@gmail.com>
2024-10-11 17:24:10 +0800

    add a moon

--- src/sphere/sphere.cc
+++ src/sphere/sphere.cc
@@ -8,9 +8,10 @@
 // https://www.songho.ca/opengl/gl_sphere.html
 
 void Sphere::fillIndices() {
+  int k1, k2;
   for (int i = 0; i < stack_count_; ++i) {
-    int k1 = i * (sector_count_ + 1); // beginning of current stack
-    int k2 = k1 + sector_count_ + 1;  // beginning of next stack
+    k1 = i * (sector_count_ + 1); // beginning of current stack
+    k2 = k1 + sector_count_ + 1;  // beginning of next stack
 
     for (int j = 0; j < sector_count_; ++j, ++k1, ++k2) {
       // 2 triangles per sector excluding first and last stacks
@@ -32,8 +33,8 @@ void Sphere::fillIndices() {
       // vertical lines for all stacks, k1 => k2
       line_indices_.push_back(k1);
       line_indices_.push_back(k2);
-      if (i != 0) {
-        // horizontal lines except 1st stack, k1 => k+1
+      if (i != 0) // horizontal lines except 1st stack, k1 => k+1
+      {
         line_indices_.push_back(k1);
         line_indices_.push_back(k1 + 1);
       }
@@ -43,25 +44,34 @@ void Sphere::fillIndices() {
 
 void Sphere::fillVertices() {
   const float PI = std::acos(-1.0f);
-  const float sector_step = 2 * PI / sector_count_;
-  const float stack_step = PI / stack_count_;
+  float x, y, z, xy;                            // vertex position
+  float nx, ny, nz, lengthInv = 1.0f / radius_; // vertex normal
 
-  for (int i = 0; i <= stack_count_; i++) {
-    float const stack_angle = PI / 2 - i * stack_step;
-    float const xy = radius_ * std::cos(stack_angle);
-    float const z = radius_ * std::sin(stack_angle);
+  float sectorStep = 2 * PI / sector_count_;
+  float stackStep = PI / stack_count_;
+  float sectorAngle, stackAngle;
 
+  for (int i = 0; i <= stack_count_; ++i) {
+    stackAngle = PI / 2 - i * stackStep; // starting from pi/2 to -pi/2
+    xy = radius_ * cosf(stackAngle);     // r * cos(u)
+    z = radius_ * sinf(stackAngle);      // r * sin(u)
+
+    // add (sectorCount+1) vertices per stack
+    // first and last vertices have same position and normal, but different tex
+    // coords
     for (int j = 0; j <= sector_count_; ++j) {
-      float const sector_angle = j * sector_step; // starting from 0 to 2pi
+      sectorAngle = j * sectorStep; // starting from 0 to 2pi
 
       // vertex position (x, y, z)
-      float const x = xy * cosf(sector_angle); // r * cos(u) * cos(v)
-      float const y = xy * sinf(sector_angle); // r * cos(u) * sin(v)
-
+      x = xy * cosf(sectorAngle); // r * cos(u) * cos(v)
+      y = xy * sinf(sectorAngle); // r * cos(u) * sin(v)
       // normalized vertex normal (nx, ny, nz)
-      normal_vertices_.push_back(x / radius_);
-      normal_vertices_.push_back(y / radius_);
-      normal_vertices_.push_back(z / radius_);
+      nx = x * lengthInv;
+      ny = y * lengthInv;
+      nz = z * lengthInv;
+      normal_vertices_.push_back(nx);
+      normal_vertices_.push_back(ny);
+      normal_vertices_.push_back(nz);
     }
   }
 }
@@ -156,7 +166,6 @@ void Sphere::programDataSet() {
                      glm::value_ptr(projection));
 }
 
-
 void Sphere::drawMainloop() {
   // mainloop;
   glEnable(GL_DEPTH_TEST);
@@ -169,6 +178,8 @@ void Sphere::drawMainloop() {
     FrameCount();
 
     processInput(window_);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // clear the screen first
     glClearColor(0.2f, 0.2f, 0.2f, 0.8f);
@@ -183,14 +194,17 @@ void Sphere::drawMainloop() {
     glBindVertexArray(VAO_);
     // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // The first argument specifies the mode we want to draw in, similar to
-    // glDrawArrays. The second argument is the count or number of elements we'd
-    // like to draw. We specified 6 indices so we want to draw 6 vertices in
-    // total. The third argument is the type of the indices which is of type
-    // GL_UNSIGNED_INT. The last argument allows us to specify an offset in the
-    // EBO (or pass in an index array, but that is when you're not using element
-    // buffer objects), but we're just going to leave this at 0.
-    //  glDrawElements(GL_TRIANGLES, , sizeof(vertices) / sizeof(vertices[0]));
-    glDrawElements(GL_TRIANGLES, static_cast<unsigned>(indices_.size()), GL_UNSIGNED_INT, 0);
+    // glDrawArrays. The second argument is the count or number of elements
+    // we'd like to draw. We specified 6 indices so we want to draw 6
+    // vertices in total. The third argument is the type of the indices
+    // which is of type GL_UNSIGNED_INT. The last argument allows us to
+    // specify an offset in the EBO (or pass in an index array, but that is
+    // when you're not using element buffer objects), but we're just going
+    // to leave this at 0.
+    //  glDrawElements(GL_TRIANGLES, , sizeof(vertices) /
+    //  sizeof(vertices[0]));
+    glDrawElements(GL_TRIANGLES, static_cast<unsigned>(indices_.size()),
+                   GL_UNSIGNED_INT, 0);
 
     updateVertices();
 
